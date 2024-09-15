20806264862845c29f2593b05f7fd62ff15a6477
Wellebye <William@ellebye.com>
2024-09-15 18:04:35 +0200

    Multiplayer
    TypeBeat

---
+++ Source/PilotProjekt_DDU/PilotProjekt_DDU.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PilotProjekt_DDU.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PilotProjekt_DDU, "PilotProjekt_DDU" );
